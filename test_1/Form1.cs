7ab54369f92aa3c493604c60e8e315014c2f265a
Fanfir <daridep189@student.polsl.pl>
2016-10-07 16:29:03 +0200

    1234555

--- test_1/Form1.cs
+++ test_1/Form1.cs
@@ -16,5 +16,10 @@ namespace test_1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "EXIT";
+        }
     }
 }
